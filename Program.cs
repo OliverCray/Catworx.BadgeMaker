37555607d21c836c962e4748627eb3339b468ea1
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2024-01-15 16:43:52 +0000

    Created GetEmployees method

--- Program.cs
+++ Program.cs
@@ -7,22 +7,29 @@ namespace CatWorx.BadgeMaker
   {
     static void Main(string[] args)
     {
-      List<string> employees = new List<string>();
+      List<string> employees = GetEmployees();
 
-      while (true)
+      for (int i = 0; i < employees.Count; i++)
       {
-        Console.WriteLine("Please enter a name: ");
-        string input = Console.ReadLine() ?? "";
-        if (input == "")
-        {
-          break;
-        }
-        employees.Add(input);
-      }
-      
-      for (int i = 0; i < employees.Count; i++) {
         Console.WriteLine(employees[i]);
-      }
+      } 
     }
+
+    static List<string> GetEmployees()
+      {
+        List<string> employees = new List<string>();
+        while (true)
+        {
+          Console.WriteLine("Please enter a name: (leave empty to exit): ");
+          string input = Console.ReadLine() ?? "";
+          if (input == "")
+          {
+            break;
+          }
+
+          employees.Add(input);
+        }
+        return employees;
+      }
   }
 }
