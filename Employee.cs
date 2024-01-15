faaf2d4f95de672602b19b9fd7f5a44a4a3f9d11
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2024-01-15 17:43:01 +0000

    Added properties to Employee class

--- Employee.cs
+++ Employee.cs
@@ -2,6 +2,13 @@ namespace CatWorx.BadgeMaker
 {
   class Employee
   {
-    
+    public string FirstName;
+    public string LastName;
+    public int Id;
+    public string PhotoUrl;
+    public Employee(string firstName) 
+    {
+      FirstName = firstName;
+    }
   }
 }
