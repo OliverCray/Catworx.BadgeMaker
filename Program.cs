c9e849066b5a97f2ac011291e30eb9d240c02dc7
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2024-01-15 16:17:18 +0000

    Added simple loop to read and write employee names

--- Program.cs
+++ Program.cs
@@ -7,7 +7,22 @@ namespace CatWorx.BadgeMaker
   {
     static void Main(string[] args)
     {
+      List<string> employees = new List<string>();
+
+      while (true)
+      {
+        Console.WriteLine("Please enter a name: ");
+        string input = Console.ReadLine() ?? "";
+        if (input == "")
+        {
+          break;
+        }
+        employees.Add(input);
+      }
       
+      for (int i = 0; i < employees.Count; i++) {
+        Console.WriteLine(employees[i]);
+      }
     }
   }
 }
